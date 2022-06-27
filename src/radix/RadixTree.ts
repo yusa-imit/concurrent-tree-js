b20db96a233b8e61e74aa6cf3ae01a93bcc19827
yusa-imit <esimileiar81@gmail.com>
2022-06-27 18:36:43 +0900

    add radix tree interface

---
+++ src/radix/RadixTree.ts
@@ -0,0 +1,15 @@
+import { KeyValuePair } from "../common/KeyValuePair";
+
+export interface RadixTree<T> {
+  put(key: string, value: T): T;
+  putIfAbsent(key: string, value: T): T;
+  remove(key: string): boolean;
+  getValueForExactKey(key: string): T;
+  getKeyStartingWith(prefix: string): Iterable<string>;
+  getValuesForKeysStartingWith(prefix: string): Iterable<T>;
+  getKeyValuePairsForKeysStartingWith(perfix: string): Iterable<KeyValuePair<T>>;
+  getClosestKeys(candidate: string): Iterable<string>;
+  getValuesForClosestKeys(candidate: string): Iterable<T>;
+  getKeyValuePairsForClosestKeys(candidate: string): Iterable<KeyValuePair<T>>;
+  size(): number;
+}
