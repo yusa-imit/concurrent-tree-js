2ee559d630cb671a64539ed628e493671699b60e
yusa-imit <esimileiar81@gmail.com>
2022-07-19 19:01:20 +0900

    complete strings

--- src/common/Strings.ts
+++ src/common/Strings.ts
@@ -1,17 +1,20 @@
 export namespace Strings {
   export function generateSuffixes(input: string): Iterable<string> {
     class itr implements IterableIterator<string> {
-      idx: number = 1;
+      idx: number = 0;
       _arr: string;
       constructor(_a: string) {
         this._arr = _a;
       }
       next(): IteratorResult<string, any> {
         if (!this.hasNext()) return { value: undefined, done: true };
-        return { value: this._arr.slice(0, this.idx++), done: false };
+        return {
+          value: this._arr.slice(this.idx++, this._arr.length),
+          done: false,
+        };
       }
       hasNext(): boolean {
-        return this.idx <= this._arr.length + 1;
+        return this.idx < this._arr.length;
       }
       [Symbol.iterator]() {
         return this;
@@ -20,7 +23,68 @@ export namespace Strings {
     return new itr(input);
   }
   export function generatePrefixes(input: string): Iterable<string> {
-    return;
+    class itr implements IterableIterator<string> {
+      idx: number = 0;
+      _arr: string;
+      constructor(_a: string) {
+        this._arr = _a;
+      }
+      next(): IteratorResult<string, any> {
+        if (!this.hasNext()) return { value: undefined, done: true };
+        return { value: this._arr.slice(0, ++this.idx), done: false };
+      }
+      hasNext(): boolean {
+        return this.idx < this._arr.length; // edit here
+      }
+      [Symbol.iterator]() {
+        return this;
+      }
+    }
+    return new itr(input);
+  }
+  export function getCommonPrefix(first: string, second: string) {
+    const minLength = Math.min(first.length, second.length);
+    for (var i = 0; i < minLength; i++) {
+      if (first.charAt(i) !== second.charAt(i)) {
+        return first.slice(0, i);
+      }
+    }
+    return first.slice(0, minLength);
+  }
+  export function getSuffix(input: string, startIndex: number) {
+    if (startIndex >= input.length) {
+      return '';
+    }
+    return input.slice(startIndex, input.length);
+  }
+  export function getPrefix(input: string, endIndex: number) {
+    if (endIndex < input.length) {
+      return input;
+    }
+    return input.slice(0, endIndex);
+  }
+  export function subtractPrefix(main: string, prefix: string) {
+    const startIndex = prefix.length;
+    const mainLength = main.length;
+    if (startIndex > mainLength) {
+      return '';
+    }
+    return main.slice(startIndex, mainLength);
+  }
+  export function concatenate(first: string, second: string) {
+    return first.concat(second);
+  }
+  export function reverse(input: string) {
+    return input
+      .split('')
+      .reverse()
+      .join('');
+  }
+  export function fromCharArray(input: string[]) {
+    return input.join('');
+  }
+  export function toCharArray(input: string) {
+    return input.split('');
   }
 }
 
